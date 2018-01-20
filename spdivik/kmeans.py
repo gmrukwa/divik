1c378d4fd753fb2f45a878e6d3bc2836805a0f07
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-20 22:52:17 +0100

    Define type aliases

--- spdivik/kmeans.py
+++ spdivik/kmeans.py
@@ -1,3 +1,11 @@
 """Numpy-based implementation of k-means algorithm"""
+from abc import ABCMeta, abstractmethod
 
 import numpy as np
+
+import spdivik.distance as dist
+
+
+Labels = np.ndarray
+Data = np.ndarray
+Centroids = np.ndarray
