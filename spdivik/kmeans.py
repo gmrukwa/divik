638b06a86a129b95e765e835a2d8d1d5d44bda53
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-20 22:52:34 +0100

    Add Initialization

--- spdivik/kmeans.py
+++ spdivik/kmeans.py
@@ -9,3 +9,17 @@ import spdivik.distance as dist
 Labels = np.ndarray
 Data = np.ndarray
 Centroids = np.ndarray
+
+
+class Initialization(object, metaclass=ABCMeta):
+    """Initializes k-means algorithm"""
+    @abstractmethod
+    def __call__(self, data: Data, number_of_centroids: int) -> Centroids:
+        """Generate initial centroids for k-means algorithm
+
+        @param data: 2D matrix with observations in rows, features in columns
+        @param number_of_centroids: number of centroids to be generated
+        @return: centroids, in rows
+        """
+        raise NotImplementedError(self.__class__.__name__
+                                  + " must implement __call__.")
