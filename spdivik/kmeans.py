6f8551255bf4dffa288d443a14885dde861fadab
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-22 14:25:34 +0100

    Add ExtremeInitialization

--- spdivik/kmeans.py
+++ spdivik/kmeans.py
@@ -23,3 +23,39 @@ class Initialization(object, metaclass=ABCMeta):
         """
         raise NotImplementedError(self.__class__.__name__
                                   + " must implement __call__.")
+
+
+class ExtremeInitialization(Initialization):
+    """Initializes k-means by picking extreme points"""
+    def __init__(self, distance: dist.DistanceMetric):
+        self.distance = distance
+
+    def __call__(self, data: Data, number_of_centroids: int) -> Centroids:
+        """Generate initial centroids for k-means algorithm
+
+        @param data: 2D matrix with observations in rows, features in columns
+        @param number_of_centroids: number of centroids to be generated
+        @return: centroids, in rows
+        """
+        if number_of_centroids > data.shape[0]:
+            raise ValueError("Number of centroids (%i) greater than number of "
+                             "observations (%i)."
+                             % (number_of_centroids, data.shape[0]))
+        features = data.T
+        assumed_ys = features[0]
+        modelled_xs = np.hstack([np.ones((data.shape[0], 1)),
+                                 features[1:].T])
+        default_singular_value_threshold = -1
+        coefficients, _, _, _ = np.linalg.lstsq(
+            modelled_xs, assumed_ys, rcond=default_singular_value_threshold)
+        residuals = np.abs(np.dot(modelled_xs, coefficients) - assumed_ys)
+
+        centroids = np.nan * np.zeros((number_of_centroids, data.shape[1]))
+        centroids[0] = data[np.argmax(residuals)]
+
+        for i in range(1, number_of_centroids):
+            distances = self.distance(data, centroids[0:i])
+            distances = np.min(distances, axis=1)
+            centroids[i] = data[np.argmax(distances)]
+
+        return centroids
--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -16,3 +16,40 @@ data = np.array([
     [1.1, 0.8, 1.1, 1.1],
     [1000, 490231, -412342, -7012]
 ])
+
+
+class ExtremeInitializationTest(unittest.TestCase):
+    def setUp(self):
+        self.number_of_clusters = 2
+        self.distance = dist.ScipyDistance(dist.KnownMetric.euclidean)
+        self.initialize = km.ExtremeInitialization(self.distance)
+
+    def test_uses_given_distance(self):
+        with patch.object(dist.ScipyDistance, '__call__') as mock:
+            self.initialize(data, self.number_of_clusters)
+            self.assertGreater(mock.call_count, 0)
+
+    def test_centroids_have_the_same_number_of_features_as_data(self):
+        centroids = self.initialize(data, self.number_of_clusters)
+        self.assertEqual(centroids.shape[1], data.shape[1])
+
+    def test_number_of_centroids_is_preserved(self):
+        centroids = self.initialize(data, self.number_of_clusters)
+        self.assertEqual(centroids.shape[0], self.number_of_clusters)
+
+    def test_works_without_error_for_ill_conditioned_problems(self):
+        self.initialize(data[0:3], self.number_of_clusters)
+
+    def test_throws_when_required_more_centroids_than_data(self):
+        with self.assertRaises(ValueError):
+            self.initialize(data[0:self.number_of_clusters-1],
+                            self.number_of_clusters)
+
+    def test_first_centroid_is_furthest_from_all(self):
+        centroid = self.initialize(data, 1)
+        np.testing.assert_equal(centroid, data[-1].reshape(1, -1))
+
+    def test_next_centroid_is_furthest_from_already_found(self):
+        centroids = self.initialize(data, 3)
+        np.testing.assert_equal(centroids[1], data[4])
+        np.testing.assert_equal(centroids[2], data[2])
