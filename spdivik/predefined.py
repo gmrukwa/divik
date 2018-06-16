3d37564d9f5740c6de6aa42fef65a791ab299608
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-06-16 22:47:11 +0200

    Add predefined scenario for master thesis
    Filters by amplitude on each level and uses Gap statistic as stop condition

--- spdivik/predefined.py
+++ spdivik/predefined.py
@@ -1,4 +1,5 @@
 from functools import partial
+from multiprocessing import Pool
 from typing import Callable, Optional
 
 import spdivik.distance as dst
@@ -12,13 +13,20 @@ import spdivik.types as ty
 Divik = Callable[[ty.Data], Optional[dv.DivikResult]]
 
 
-def _dunn_optimized_kmeans(metric: dst.KnownMetric,
-                           iters_limit: int=100) -> sc.Optimizer:
+def _extreme_kmeans(metric: dst.KnownMetric,
+                    iters_limit: int = 100) -> km.KMeans:
     distance = dst.ScipyDistance(metric)
-    dunn = partial(sc.dunn, distance=distance)
     kmeans = km.KMeans(labeling=km.Labeling(distance),
                        initialize=km.ExtremeInitialization(distance),
                        number_of_iterations=iters_limit)
+    return kmeans
+
+
+def _dunn_optimized_kmeans(metric: dst.KnownMetric,
+                           iters_limit: int = 100) -> sc.Optimizer:
+    distance = dst.ScipyDistance(metric)
+    dunn = partial(sc.dunn, distance=distance)
+    kmeans = _extreme_kmeans(metric, iters_limit)
     sweep_clusters_number = [
         sc.ParameterValues('number_of_clusters', list(range(2, 11)))
     ]
@@ -28,18 +36,22 @@ def _dunn_optimized_kmeans(metric: dst.KnownMetric,
     return best_kmeans_with_dunn
 
 
-_PROTEOMIC_DENOISE_SETTING = {
-    'statistic': fs.amplitude,
-    'discard_up_to': 1,  # discarding only lowest component, if possible
-    'preserve_topmost': True,
-    'min_features_percentage': .05
-}
-_PROTEOMIC_FILTER_SETTING = {
-    'statistic': fs.variance,
-    'discard_up_to': -1,  # selecting only most varying component, if possible
-    'preserve_topmost': True,
-    'min_features_percentage': .05
-}
+_AMPLITUDE_FILTER = dv.FilteringMethod(
+    'amplitude',
+    partial(fs.select_by,
+            statistic=fs.amplitude,
+            discard_up_to=1,
+            # discarding only lowest component, if possible
+            preserve_topmost=True,
+            min_features_percentage=.05))
+_VARIANCE_FILTER = dv.FilteringMethod(
+    'variance',
+    partial(fs.select_by,
+            statistic=fs.variance,
+            # selecting only most varying component, if possible
+            discard_up_to=-1,
+            preserve_topmost=True,
+            min_features_percentage=.05))
 
 
 class _PrefilteringWrapper:
@@ -55,20 +67,30 @@ class _PrefilteringWrapper:
         return result
 
 
-def proteomic(minimal_split_segment: int = 20, iters_limit: int=100) -> Divik:
-    amplitude_filter = partial(fs.select_by, **_PROTEOMIC_DENOISE_SETTING)
-    noise_removal_strategy = dv.FilteringMethod('amplitude', amplitude_filter)
-
+def proteomic(minimal_split_segment: int = 20, iters_limit: int = 100) -> Divik:
     best_kmeans_with_dunn = _dunn_optimized_kmeans(
         dst.KnownMetric.correlation, iters_limit)
-    variance_filter = partial(fs.select_by, **_PROTEOMIC_FILTER_SETTING)
-    variance_strategy = dv.FilteringMethod('variance', variance_filter)
     stop_for_small_size = partial(st.minimal_size, size=minimal_split_segment)
     divik = partial(dv.divik,
                     split=best_kmeans_with_dunn,
-                    feature_selectors=[variance_strategy],
+                    feature_selectors=[_VARIANCE_FILTER],
                     stop_condition=stop_for_small_size)
-
-    prefiltered_divik = _PrefilteringWrapper(prefilter=noise_removal_strategy,
+    prefiltered_divik = _PrefilteringWrapper(prefilter=_AMPLITUDE_FILTER,
                                              divik=divik)
     return prefiltered_divik
+
+
+def master(gap_trials: int=100, iters_limit: int = 100, pool: Pool=None) -> Divik:
+    metric = dst.KnownMetric.correlation
+    best_kmeans_with_dunn = _dunn_optimized_kmeans(metric, iters_limit)
+    fast_kmeans = partial(_extreme_kmeans(metric, iters_limit=10),
+                          number_of_clusters=2)
+    distance = dst.ScipyDistance(metric)
+    stop_if_split_makes_no_sense = st.combine(
+        partial(st.minimal_size, size=20),
+        st.Gap(distance, fast_kmeans, gap_trials, pool=pool))
+    divik = partial(dv.divik,
+                    split=best_kmeans_with_dunn,
+                    feature_selectors=[_AMPLITUDE_FILTER, _VARIANCE_FILTER],
+                    stop_condition=stop_if_split_makes_no_sense)
+    return divik
--- test/test_predefined.py
+++ test/test_predefined.py
@@ -1,23 +1,37 @@
 import unittest
+from multiprocessing import Pool
 
 import numpy as np
 
 import spdivik.predefined as pre
 
 
+N_OBSERVATIONS = 200
+N_FEATURES = 40
+N_VARYING = 10
+N_UPREGULATED = 20
+N_SEPARATED = 100
+
+
 class DataBoundTestCase(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         np.random.seed(0)
-        self.data = np.random.randn(300, 100)
+        cls.data = np.random.randn(N_OBSERVATIONS, N_FEATURES)
         # vary some features
-        idx = np.random.randint(0, 100, 20)
-        self.data[:, tuple(idx)] *= 3
+        idx = np.random.randint(0, N_FEATURES, N_VARYING)
+        cls.data[:, tuple(idx)] *= 3
         # upregulate some features
-        idx = np.random.randint(0, 100, 80)
-        self.data[:, tuple(idx)] += 100
+        idx = np.concatenate((
+            np.random.randint(0, N_FEATURES, N_UPREGULATED - N_VARYING),
+            idx))
+        cls.data[:, tuple(idx)] += 100
         # separate a group
-        idx = np.random.randint(0, 300, 100)
-        self.data[tuple(idx), :] += 100
+        idx = np.random.randint(0, N_OBSERVATIONS, N_SEPARATED)
+        cls.data[tuple(idx), :] += 500
+        # normalize data
+        cls.data = cls.data - np.mean(cls.data, axis=0)
+        cls.data = cls.data / np.max(cls.data, axis=0)
 
 
 class TestProteomic(DataBoundTestCase):
@@ -42,3 +56,27 @@ class TestProteomic(DataBoundTestCase):
         divik = pre.proteomic(minimal_split_segment=10)
         some_result = divik(self.data)
         self.assertFalse(np.isnan(some_result.quality))
+
+
+pool = Pool()
+
+
+class TestMaster(DataBoundTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestMaster, cls).setUpClass()
+        divik = pre.master(gap_trials=1000, pool=pool)
+        cls.result = divik(cls.data)
+
+    def test_constructs_runnable_pipeline(self):
+        self.assertIsNotNone(self.result)
+
+    def test_splits_test_data_into_two_topmost_groups(self):
+        self.assertEqual(len(self.result.subregions), 2)
+
+    def test_makes_only_topmost_split(self):
+        self.assertIsNone(self.result.subregions[0])
+        self.assertIsNone(self.result.subregions[1])
+
+    def test_scores_segmentation(self):
+        self.assertFalse(np.isnan(self.result.quality))
