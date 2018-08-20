c13ca2a26d6e391414a6a200fbbd6e171a45d6d9
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-08-20 20:01:36 +0200

    Scatter experiment construction

--- spdivik/kmeans/__main__.py
+++ spdivik/kmeans/__main__.py
@@ -23,54 +23,89 @@ Gap = Callable[
     [ty.Data, ty.IntLabels, ty.Centroids, ty.SegmentationMethod],
     float
 ]
+Min, Max = int, int
 Experiment = NamedTuple('Experiment', [
     ('kmeans', km.KMeans),
     ('gap', Gap),
-    ('maximal_number_of_clusters', int),
-    ('minimal_number_of_clusters', int),
+    ('maximal_number_of_clusters', Max),
+    ('minimal_number_of_clusters', Min),
     ('gap_pool', Pool),
     ('grouping_pool', Optional[Pool])
 ])
 
 
-def build_experiment(config) -> Experiment:
+def parse_distance(config) -> dst.ScipyDistance:
     assert_configured(config, 'distance')
     known_distances = {metric.value: metric for metric in dst.KnownMetric}
     assert config['distance'] in known_distances, \
         'Unknown distance {0}. Known: {1}'.format(
-        config['distance'], list(known_distances.keys()))
+            config['distance'], list(known_distances.keys()))
     distance = dst.ScipyDistance(known_distances[config['distance']])
-    labeling = km.Labeling(distance)
+    return distance
+
+
+def parse_initialization(config, distance: dst.DistanceMetric) \
+        -> km.PercentileInitialization:
     assert_configured(config, 'initialization_percentile')
     initialization_percentile = float(config['initialization_percentile'])
     assert 0 <= initialization_percentile <= 100
     initialization = km.PercentileInitialization(
         distance=distance, percentile=initialization_percentile)
+    return initialization
+
+
+def parse_number_of_iterations(config) -> int:
     assert_configured(config, 'number_of_iterations')
     number_of_iterations = int(config['number_of_iterations'])
     assert 0 <= number_of_iterations
-    kmeans = km.KMeans(labeling=labeling, initialize=initialization,
-                       number_of_iterations=number_of_iterations)
-    assert_configured(config, 'gap_trials')
-    gap_trials = int(config['gap_trials'])
-    assert 0 <= gap_trials
+    return number_of_iterations
+
+
+def parse_clustering_limits(config) -> Tuple[Min, Max]:
     assert_configured(config, 'maximal_number_of_clusters')
     maximal_number_of_clusters = int(config['maximal_number_of_clusters'])
-    assert 1 < maximal_number_of_clusters
+    assert 1 < maximal_number_of_clusters, maximal_number_of_clusters
     minimal_number_of_clusters = int(config.get('minimal_number_of_clusters', 1))
-    assert 0 < minimal_number_of_clusters < maximal_number_of_clusters
+    assert 0 < minimal_number_of_clusters < maximal_number_of_clusters, \
+        (minimal_number_of_clusters, maximal_number_of_clusters)
+    return minimal_number_of_clusters, maximal_number_of_clusters
+
+
+def spawn_pool(config) -> Tuple[Pool, Optional[Pool]]:
     pool_max_tasks = int(config.get('pool_max_tasks', 4))
     assert 0 <= pool_max_tasks
     pool = Pool(maxtasksperchild=pool_max_tasks)
     use_pool_for_grouping = bool(config.get('use_pool_for_grouping', False))
-    gap = partial(sc.gap, distance=distance, n_trials=gap_trials, pool=pool,
+    return pool, pool if use_pool_for_grouping else None
+
+
+def parse_gap_trials(config) -> int:
+    assert_configured(config, 'gap_trials')
+    gap_trials = int(config['gap_trials'])
+    assert 0 <= gap_trials
+    return gap_trials
+
+
+def build_experiment(config) -> Experiment:
+    k_min, k_max = parse_clustering_limits(config)
+    distance = parse_distance(config)
+    labeling = km.Labeling(distance)
+    initialization = parse_initialization(config, distance)
+    number_of_iterations = parse_number_of_iterations(config)
+    kmeans = km.KMeans(labeling=labeling, initialize=initialization,
+                       number_of_iterations=number_of_iterations)
+
+    gap_pool, grouping_pool = spawn_pool(config)
+
+    gap_trials = parse_gap_trials(config)
+    gap = partial(sc.gap, distance=distance, n_trials=gap_trials, pool=gap_pool,
                   return_deviation=True)
     return Experiment(kmeans=kmeans,
                       gap=gap,
-                      maximal_number_of_clusters=maximal_number_of_clusters,
-                      minimal_number_of_clusters=minimal_number_of_clusters,
-                      gap_pool=pool,
-                      grouping_pool=pool if use_pool_for_grouping else None)
+                      maximal_number_of_clusters=k_max,
+                      minimal_number_of_clusters=k_min,
+                      gap_pool=gap_pool,
+                      grouping_pool=grouping_pool)
 
 
 def split_into_one(data: ty.Data) -> Tuple[ty.IntLabels, ty.Centroids]:
