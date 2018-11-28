57ec5ae55329c48ae41ef4e12ca07f32addfcf51
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-11-28 20:49:03 +0100

    Add visualization to saving pipeline

--- spdivik/__main__.py
+++ spdivik/__main__.py
@@ -9,10 +9,12 @@ import typing
 import numpy as np
 import pandas as pd
 import tqdm
+import skimage.io as sio
 import spdivik.predefined as pred
 import spdivik.summary as _smr
 import spdivik.types as ty
 import spdivik._scripting as sc
+import spdivik.visualize as vis
 
 
 def build_experiment(config, data: np.ndarray) -> typing.Tuple[pred.Divik, tqdm.tqdm]:
@@ -54,7 +56,25 @@ def _make_merged(result: typing.Optional[ty.DivikResult]) -> np.ndarray:
     )
 
 
-def save(data: ty.Data, result: typing.Optional[ty.DivikResult], destination: str):
+def _save_merged(destination: str, merged: np.ndarray, xy: np.ndarray=None):
+    np.savetxt(os.path.join(destination, 'partitions.csv'),
+               merged, delimiter=', ', fmt='%i')
+    np.save(os.path.join(destination, 'partitions.npy'), merged)
+    if xy is not None:
+        x, y = xy.T
+        shape = np.max(y) + 1, np.max(x) + 1
+        for level in range(merged.shape[1]):
+            visualization = vis.visualize(merged[:, level], x, y, shape)
+            image_name = os.path.join(destination, 'partition-{0}.png'.format(level))
+            sio.imsave(image_name, visualization)
+    final_partition = merged[:, -1]
+    np.save(os.path.join(destination, 'final_partition.npy'), final_partition)
+    np.savetxt(os.path.join(destination, 'final_partition.csv'), final_partition,
+               delimiter=', ', fmt='%i')
+
+
+def save(data: ty.Data, result: typing.Optional[ty.DivikResult],
+         destination: str, xy: np.ndarray=None):
     logging.info("Saving result.")
     logging.info("Saving pickle.")
     with open(os.path.join(destination, 'result.pkl'), 'wb') as pkl:
@@ -66,15 +86,9 @@ def save(data: ty.Data, result: typing.Optional[ty.DivikResult], destination: st
         logging.info("Saving partitions.")
         merged = _make_merged(result)
         assert merged.shape[0] == result.partition.size
-        np.savetxt(os.path.join(destination, 'partitions.csv'),
-                   merged, delimiter=', ', fmt='%i')
-        np.save(os.path.join(destination, 'partitions.npy'), merged)
-        final_partition = merged[:, -1]
-        np.save(os.path.join(destination, 'final_partition.npy'), final_partition)
-        np.savetxt(os.path.join(destination, 'final_partition.csv'), final_partition,
-                   delimiter=', ', fmt='%i')
+        _save_merged(destination, merged, xy)
         logging.info("Saving centroids.")
-        centroids = pd.DataFrame(data).groupby(final_partition).mean().values
+        centroids = pd.DataFrame(data).groupby(merged[:, -1]).mean().values
         np.save(os.path.join(destination, 'centroids.npy'), centroids)
         np.savetxt(os.path.join(destination, 'centroids.csv'), centroids,
                    delimiter=', ')
