fa600e04221000558b399e2eed05540b22d22cb4
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-12-29 00:00:26 +0100

    Add arguments parsing

--- spdivik/inspect/__main__.py
+++ spdivik/inspect/__main__.py
@@ -1,9 +1,22 @@
+import argparse as agp
 
 from spdivik.inspect.app import app
 from spdivik.inspect.layout import LAYOUT
 
 
 app.layout = LAYOUT
+
+
+def parse_args():
+    parser = agp.ArgumentParser()
+
+    parser.add_argument('--result', '-r', help='divik result directory',
+                        required=True)
+    parser.add_argument('--xy', help='coordinates of points', required=True)
+    parser.add_argument('--debug', help='enables debug mode', const=True,
+                        default=False, action='store_const')
+
+    return parser.parse_args()
 
 
 def main():
