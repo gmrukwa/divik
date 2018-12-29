ecda810475a707b7bbf83e3a015e6164dc9e5489
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-12-29 11:41:37 +0100

    Visualize cluster with synthetic colors

--- spdivik/inspect/figure.py
+++ spdivik/inspect/figure.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 
 from spdivik.inspect.app import divik_result, xy
+from spdivik.inspect.color import make_colormap
 from spdivik.summary import merged_partition
 
 
@@ -36,15 +37,20 @@ def _make_default_clusters_figure():
     current = deepcopy(_DEFAULT_CLUSTERS_FIGURE)
     current['data'][0]['x'] = xy().T[0]
     current['data'][0]['y'] = xy().T[1].max() - xy().T[1]
-    current['data'][0]['z'] = merged_partition(divik_result(), 1)
     return current
+
+
+def _set_levels(level: int, current):
+    partition = merged_partition(divik_result(), level)
+    current['data'][0]['z'] = partition
+    current['data'][0]['colorscale'] = make_colormap(partition)
 
 
 def clusters_figure(level: int, title: str=None, current=None):
     if current is None:
         current = _make_default_clusters_figure()
 
-    current['data'][0]['z'] = merged_partition(divik_result(), level)
+    _set_levels(level, current)
 
     if title is not None:
         current['layout']['title'] = title
