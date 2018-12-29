d166401b460b2f920106ad6739cdaf024596c563
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-12-29 18:17:54 +0100

    Bugged: Updating exclusions

--- spdivik/inspect/exclusion.py
+++ spdivik/inspect/exclusion.py
@@ -1,9 +1,58 @@
+import json
+
 import numpy as np
 
 from spdivik.summary import merged_partition
 from spdivik.inspect.app import divik_result
 
 
+def initialize_storage(level):
+    return json.dumps({
+        'excluded': [],
+        'level': level
+    })
+
+
+def vec_in(v1, v2):
+    if np.size(v2) == 0:
+        return np.zeros((np.size(v1), 1), dtype=bool)
+    return np.max(np.reshape(v1, (-1, 1)) == np.reshape(v2, (1, -1)), axis=1)
+
+
+def update_excluded(old_level, new_level, old_excluded):
+    # if any subcluster is enabled, parent cluster will be enabled
+    # if parent is disabled, children will be disabled
+    old_partition = merged_partition(divik_result(), old_level)
+    old_enabled_flag = vec_in(old_partition, old_excluded) == 0
+
+    new_partition = merged_partition(divik_result(), new_level)
+    new_enabled_ids = np.unique(new_partition[old_enabled_flag])
+    new_disabled_ids = np.setdiff1d(new_partition, new_enabled_ids)
+
+    return list(map(int, new_disabled_ids))
+
+
+def update_storage(level, disabled_clusters, old_state):
+    state = json.loads(old_state)
+    if level == state['level']:  # user choice changed
+        state['excluded'] = disabled_clusters
+    else:                        # level changed
+        state['level'] = level
+        state['excluded'] = update_excluded(state['level'], level,
+                                            state['excluded'])
+
+    return json.dumps(state)
+
+
 def get_options(level):
     partition = merged_partition(divik_result(), level)
     return [{'label': e, 'value': e} for e in np.unique(partition)]
+
+
+def update_actual_clusters(level, storage):
+    if not storage:
+        return []
+    storage = json.loads(storage)
+    if level == storage['level']:
+        return storage['excluded']
+    return update_excluded(storage['level'], level, storage['excluded'])
--- spdivik/inspect/figure.py
+++ spdivik/inspect/figure.py
@@ -1,7 +1,5 @@
 from copy import deepcopy
 
-import numpy as np
-
 from spdivik.inspect.app import divik_result, xy
 from spdivik.inspect.color import make_colormap
 from spdivik.summary import merged_partition
@@ -43,62 +41,14 @@ def default_clusters_figure():
     current = deepcopy(_DEFAULT_CLUSTERS_FIGURE)
     current['data'][0]['x'] = xy().T[0]
     current['data'][0]['y'] = xy().T[1].max() - xy().T[1]
-    # customdata is used for filtering disabled clusters
-    # True <=> cluster enabled, False <=> cluster disabled
-    current['data'][0]['customdata'] = np.ones_like(xy().T[0])
-    set_visualization_levels(1, current)
+    update_clusters_figure(1, [], current)
     return current
 
 
-def get_enabled_ids(figure=None, enabled_flag=None, partition=None):
-    if partition is None:
-        assert figure is not None
-        partition = np.array(figure['data'][0]['z'], dtype=int)
-    if enabled_flag is None:
-        assert figure is not None
-        enabled_flag = np.array(figure['data'][0]['customdata'], dtype=bool)
-    return np.unique(partition[enabled_flag])
-
-
-def get_disabled_ids(figure=None, enabled_flag=None, partition=None):
-    if partition is None:
-        assert figure is not None
-        partition = np.array(figure['data'][0]['z'], dtype=int)
-    if enabled_flag is None:
-        assert figure is not None
-        enabled_flag = np.array(figure['data'][0]['customdata'], dtype=bool)
-    enabled_ids = get_enabled_ids(enabled_flag=enabled_flag,
-                                  partition=partition)
-    return np.setdiff1d(partition, enabled_ids)
-
-
-def get_all_labels(figure):
-    return np.unique(figure['data'][0]['z'])
-
-
-def vec_in(v1, v2):
-    return np.max(v1.reshape(-1, 1) == v2.reshape(1, -1), axis=1)
-
-
-def recompute_disabled(partition, old_enabled_flag):
-    # if any subcluster is enabled, parent cluster will be enabled
-    # if parent is disabled, children will be disabled
-    new_enabled_id = get_enabled_ids(enabled_flag=old_enabled_flag,
-                                     partition=partition)
-    new_disabled_id = get_disabled_ids(enabled_flag=old_enabled_flag,
-                                       partition=partition)
-    new_enabled_flag = vec_in(partition, new_enabled_id)
-    return new_enabled_flag, new_disabled_id
-
-
-def set_visualization_levels(level: int, current):
+def update_clusters_figure(level: int, disabled, current):
     partition = merged_partition(divik_result(), level)
-    enabled_flag, disabled_id = recompute_disabled(
-        partition, current['data'][0]['customdata'] != 0)
 
     current['data'][0]['z'] = partition
-    current['data'][0]['colorscale'] = make_colormap(partition,
-                                                     disabled=disabled_id)
-    current['data'][0]['customdata'] = enabled_flag
+    current['data'][0]['colorscale'] = make_colormap(partition, disabled=disabled)
 
     return current
