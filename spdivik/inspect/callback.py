e1ce6a42702d6f300833bf535f23b8f329dc1225
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-12-30 03:15:53 +0100

    Add callbacks required for persistent renders

--- spdivik/inspect/callback.py
+++ spdivik/inspect/callback.py
@@ -7,6 +7,7 @@ import spdivik.inspect.recolor as recolor
 import spdivik.inspect.exclusion as ex
 import spdivik.inspect.figure as fig
 from spdivik.inspect.layout import Fields
+import spdivik.inspect.persistence as per
 
 
 @app.callback(
@@ -26,12 +27,22 @@ def update_visualization(depth, disabled_state, color_overrides, current_figure)
 
 @app.callback(
     Output(Fields.DISABLED_CLUSTERS_STORAGE, 'children'),
-    [Input(Fields.DISABLED_CLUSTERS_PICKER, 'value')],
-    [State(Fields.LEVEL, 'value'), State(Fields.DISABLED_CLUSTERS_STORAGE, 'children')]
+    [
+        Input(Fields.DISABLED_CLUSTERS_PICKER, 'value'),
+        Input(Fields.LOAD_PROFILE, 'n_clicks'),
+    ],
+    [
+        State(Fields.LEVEL, 'value'),
+        State(Fields.DISABLED_CLUSTERS_STORAGE, 'children'),
+        State(Fields.SAVED_PROFILES, 'value'),
+    ]
 )
-def update_disabled_clusters_to_new_level(disabled_clusters, level, old_state):
+def update_disabled_clusters_to_new_level(disabled_clusters, _, level,
+                                          old_state, name):
     if not old_state:
         return ex.initialize_storage(level)
+    if name:
+        return per.restore_disabled_clusters(name)
     return ex.update_storage(level, disabled_clusters, old_state)
 
 
@@ -114,6 +125,7 @@ def set_r_from_point(selected_point, figure):
     [
         Input(Fields.CLUSTER_COLOR_APPLY, 'n_clicks_timestamp'),
         Input(Fields.CLUSTER_COLOR_RESET, 'n_clicks_timestamp'),
+        Input(Fields.LOAD_PROFILE, 'n_clicks_timestamp'),
     ],
     [
         State(Fields.COLOR_OVERRIDES_STORAGE, 'children'),
@@ -122,9 +134,57 @@ def set_r_from_point(selected_point, figure):
         State(Fields.CLUSTER_COLOR_B, 'value'),
         State(Fields.LEVEL, 'value'),
         State(Fields.SELECTED_POINT, 'children'),
+        State(Fields.SAVED_PROFILES, 'value')
     ]
 )
-def store_color_override(apply, reset, overrides, r, g, b, level, selected_point):
-    if apply and reset and apply < reset:
+def store_color_override(apply, reset, load, overrides, r, g, b, level,
+                         selected_point, name):
+    apply = apply or 0
+    reset = reset or 0
+    load = load or 0
+    if apply < load and reset < load:
+        if name:
+            return per.restore_color_overrides(name)
         return '{}'
-    return recolor.update_color_overrides(overrides, r, g, b, level, selected_point)
+    if apply < reset and load < reset:
+        return '{}'
+    return recolor.update_color_overrides(overrides, r, g, b, level,
+                                          selected_point)
+
+
+@app.callback(
+    Output(Fields.SAVED_PROFILES, 'options'),
+    [Input(Fields.SAVE_PROFILE, 'n_clicks')],
+    [
+        State(Fields.NEW_PROFILE_NAME, 'value'),
+        State(Fields.DISABLED_CLUSTERS_STORAGE, 'children'),
+        State(Fields.COLOR_OVERRIDES_STORAGE, 'children'),
+        State(Fields.LEVEL, 'value'),
+    ]
+)
+def save_profile(_, name, disabled, color, level):
+    if name:
+        per.preserve_by_name(name, disabled, color, level)
+    return per.find_preserved()
+
+
+@app.callback(
+    Output(Fields.LEVEL, 'value'),
+    [Input(Fields.LOAD_PROFILE, 'n_clicks')],
+    [
+        State(Fields.SAVED_PROFILES, 'value'),
+        State(Fields.LEVEL, 'value')
+    ]
+)
+def load_level_from_profile(_, name, self):
+    if not name:
+        return self
+    return per.restore_level(name)
+#
+#
+# @app.callback(
+#     Output(Fields.SAVED_PROFILES, 'value'),
+#     [Input(Fields.LOAD_PROFILE, 'n_clicks')]
+# )
+# def erase_profile_selection_after_load(_):
+#     return None
