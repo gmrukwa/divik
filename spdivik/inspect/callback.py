99e761b4ee09d09312120de1426288bb8ae2ce2b
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-12-30 14:20:31 +0100

    Separate functionalities to tabs

--- spdivik/inspect/callback.py
+++ spdivik/inspect/callback.py
@@ -186,3 +186,23 @@ def load_level_from_profile(_, name, self):
     if not name:
         return self
     return per.restore_level(name)
+
+
+def make_tab_callback(tab_id):
+    @app.callback(
+        Output(tab_id, 'style'),
+        [Input(Fields.MENU_TABS, 'value')],
+        [State(tab_id, 'style')]
+    )
+    def update_visibility(value, style):
+        style = style or {}
+        if value == tab_id:
+            style['display'] = 'block'
+        else:
+            style['display'] = 'none'
+        return style
+
+
+make_tab_callback(Fields.COLOR_MANIPULATION_TAB)
+make_tab_callback(Fields.EXCLUSION_TAB)
+make_tab_callback(Fields.PERSISTENCE_TAB)
