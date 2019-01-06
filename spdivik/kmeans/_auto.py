3f2e5369dd3d3c935e18fbd1d2f2acccf8e87cd8
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-01-07 00:30:00 +0100

    Introduce GAP into AutoKMeans

--- spdivik/kmeans/_auto.py
+++ spdivik/kmeans/_auto.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClusterMixin, TransformerMixin
 
 from spdivik.kmeans._core import KMeans
-from spdivik.score._dunn import dunn
+from spdivik.score import DunnPicker, GapPicker
 
 
 def _fit_kmeans(data: np.ndarray, n_clusters: int, distance: str = 'euclidean',
@@ -22,8 +22,11 @@ class AutoKMeans(BaseEstimator, ClusterMixin, TransformerMixin):
     def __init__(self, min_clusters: int, max_clusters: int, n_jobs: int = 1,
                  method: str = 'dunn', distance: str = 'euclidean',
                  init: str = 'percentile', percentile: float = 95.,
-                 max_iter: int = 100, normalize_rows: bool = False):
+                 max_iter: int = 100, normalize_rows: bool = False,
+                 gap_max_iter: int = 10, gap_seed: int = 0,
+                 gap_trials: int = 10):
         super().__init__()
+        assert method in {'dunn', 'gap'}
         assert min_clusters <= max_clusters
         self.min_clusters = min_clusters
         self.max_clusters = max_clusters
@@ -34,6 +37,9 @@ class AutoKMeans(BaseEstimator, ClusterMixin, TransformerMixin):
         self.percentile = percentile
         self.max_iter = max_iter
         self.normalize_rows = normalize_rows
+        self.gap_max_iter = gap_max_iter
+        self.gap_seed = gap_seed
+        self.gap_trials = gap_trials
 
     def fit(self, X, y=None):
         fit_kmeans = partial(_fit_kmeans, data=X, distance=self.distance,
@@ -42,19 +48,22 @@ class AutoKMeans(BaseEstimator, ClusterMixin, TransformerMixin):
                              normalize_rows=self.normalize_rows)
         n_clusters = range(self.min_clusters, self.max_clusters + 1)
 
-        # TODO: Generalize for GAP
-        score = partial(dunn, data=X)
+        if self.method == 'dunn':
+            method = DunnPicker()
+        elif self.method == 'gap':
+            method = GapPicker(self.gap_max_iter, self.gap_seed, self.gap_trials)
+        else:
+            raise ValueError('Unknown quality measure {0}'.format(self.method))
 
         if self.n_jobs == 1:
             self.estimators_ = [fit_kmeans(n_clusters=k) for k in n_clusters]
-            self.scores_ = [score(estimator) for estimator in self.estimators_]
+            self.scores_ = method.score(X, self.estimators_)
         else:
             with Pool(self.n_jobs) as pool:
                 self.estimators_ = pool.map(fit_kmeans, n_clusters)
-                self.scores_ = pool.map(score, self.estimators_)
+                self.scores_ = method.score(X, self.estimators_, pool)
 
-        # TODO: Generalize for other scenarios
-        best = int(np.argmax(self.scores_))
+        best = method.select(self.scores_)
 
         self.n_clusters_ = best + self.min_clusters
         self.best_score_ = self.scores_[best]
--- spdivik/score/__init__.py
+++ spdivik/score/__init__.py
@@ -1,3 +1,4 @@
 from ._optimizer import Optimizer, ParameterValues
-from ._dunn import dunn_
-from ._gap import gap_
+from ._dunn import dunn_, DunnPicker
+from ._gap import gap_, GapPicker
+from ._picker import Picker
