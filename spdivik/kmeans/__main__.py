0c151cb8a5db28bc03fe95feb796ba25f1e533e5
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-01-07 08:52:01 +0100

    Visualize best clustering

--- spdivik/kmeans/__main__.py
+++ spdivik/kmeans/__main__.py
@@ -4,11 +4,15 @@ import logging
 import os
 import pickle
 from typing import List, Tuple
+
 import numpy as np
 import pandas as pd
+import skimage.io as sio
+
 import spdivik.kmeans as km
 import spdivik.types as ty
 import spdivik._scripting as scr
+import spdivik.visualize as vis
 
 
 Segmentations = List[Tuple[ty.IntLabels, ty.Centroids]]
@@ -52,7 +56,7 @@ def make_scores_report(kmeans: km.AutoKMeans) -> pd.DataFrame:
     raise ValueError(kmeans.method)
 
 
-def save(kmeans: km.AutoKMeans, destination: str):
+def save(kmeans: km.AutoKMeans, destination: str, xy: np.ndarray=None):
     logging.info("Saving result.")
 
     logging.info("Saving model.")
@@ -67,20 +71,24 @@ def save(kmeans: km.AutoKMeans, destination: str):
     np.save(fname('final_partition.npy'), kmeans.labels_.reshape(-1, 1))
 
     segmentations = get_segmentations(kmeans)
-    with open(os.path.join(destination, 'segmentations.pkl'), 'wb') as pkl:
+    with open(fname('segmentations.pkl'), 'wb') as pkl:
         pickle.dump(segmentations, pkl)
 
     partitions = make_segmentations_matrix(kmeans)
-    np.savetxt(os.path.join(destination, 'partitions.csv'), partitions,
-               delimiter=', ', fmt='%i')
+    np.savetxt(fname('partitions.csv'), partitions, delimiter=', ', fmt='%i')
 
     for i in range(partitions.shape[1]):
-        np.savetxt(os.path.join(destination, 'partitions.{0}.csv').format(i),
+        np.savetxt(fname('partitions.{0}.csv').format(i + kmeans.min_clusters),
                    partitions[:, i].reshape(-1, 1), delimiter=', ', fmt='%i')
+
+    if xy is not None:
+        visualization = vis.visualize(kmeans.labels_, xy=xy)
+        sio.imsave(fname('partitions.{0}.png').format(kmeans.n_clusters_),
+                   visualization)
 
     logging.info("Saving scores.")
     report = make_scores_report(kmeans)
-    report.to_csv(os.path.join(destination, 'scores.csv'))
+    report.to_csv(fname('scores.csv'))
 
 
 def main():
