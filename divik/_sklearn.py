0608591958423fe8fb004daf8165faa799632f59
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-10-19 21:48:01 +0200

    Add docstrings to DiviK

--- divik/_sklearn.py
+++ divik/_sklearn.py
@@ -14,6 +14,133 @@ import divik.summary as summary
 
 
 class DiviK(BaseEstimator, ClusterMixin, TransformerMixin):
+    """DiviK clustering
+
+    Parameters
+    ----------
+
+    gap_trials: int, optional, default: 10
+        The number of random dataset draws to estimate the GAP index for the
+        clustering quality assessment.
+
+    distance_percentile: float, optional, default: 99.0
+        The percentile of the distance between points and their closest
+        centroid. 100.0 would simply select the furthest point from all the
+        centroids found already. Lower value provides better robustness against
+        outliers. Too low value reduces the capability to detect centroid
+        candidates during initialization.
+
+    max_iter: int, optional, default: 100
+        Maximum number of iterations of the k-means algorithm for a single run.
+
+    distance: str, optional, default: 'correlation'
+        The distance metric between points, centroids and for GAP index
+        estimation.
+
+    minimal_size: int, optional, default: None
+        The minimum size of the region (the number of observations) to be
+        considered for any further divisions. When left None, defaults to
+        0.1% of the training dataset size.
+
+    rejection_size: int, optional, default: None
+        Size under which split will be rejected - if a cluster appears in the
+        split that is below rejection_size, the split is considered improper
+        and discarded. This may be useful for some domains (like there is no
+        justification for a 3-cells cluster in biological data). By default,
+        no segmentation is discarded, as careful post-processing provides the
+        same advantage.
+
+    rejection_percentage: float, optional, default: None
+        An alternative to ``rejection_size``, with the same behavior, but this
+        parameter is related to the training data size percentage. By default,
+        no segmentation is discarded.
+
+    minimal_features_percentage: float, optional, default: 0.01
+        The minimal percentage of features that must be preserved after
+        GMM-based feature selection. By default at least 1% of features is
+        preserved in the filtration process.
+
+    fast_kmeans_iters: int, optional, default: 10
+        Maximum number of iterations of the k-means algorithm for a single run
+        during computation of the GAP index. Decreased with respect to the
+        max_iter, as GAP index requires multiple segmentations to be evaluated.
+
+    k_max: int, optional, default: 10
+        Maximum number of clusters evaluated during the auto-tuning process.
+        From 1 up to k_max clusters are tested per evaluation.
+
+    normalize_rows: bool, optional, default: None
+        Whether to normalize each row of the data to the norm of 1. By default,
+        it normalizes rows for correlation metric, does no normalization
+        otherwise.
+
+    use_logfilters: bool, optional, default: False
+        Whether to compute logarithm of feature characteristic instead of the
+        characteristic itself. This may improve feature filtering performance,
+        depending on the distribution of features, however all the
+        characteristics (mean, variance) have to be positive for that -
+        filtering will fail otherwise. This is useful for specific cases in
+        biology where the distribution of data may actually require this option
+        for any efficient filtering.
+
+    n_jobs : int, optional, default: None
+        The number of jobs to use for the computation. This works by computing
+        each of the GAP index evaluations in parallel.
+
+    verbose : bool, optional, default: False
+        Whether to report the progress of the computations.
+
+    Attributes
+    ----------
+
+    result_ : divik.types.DivikResult
+        Hierarchical structure describing all the consecutive segmentations.
+
+    labels_ :
+        Labels of each point
+
+    centroids_ : array, [n_clusters, n_features]
+        Coordinates of cluster centers. If the algorithm stops before fully
+        converging, these will not be consistent with ``labels_``. Also, the
+        distance between points and respective centroids must be captured
+        in appropriate features subspace. This is realized by the ``transform``
+        method.
+
+    depth_ : int
+        The number of hierarchy levels in the segmentation.
+
+    n_clusters_ : int
+        The final number of clusters in the segmentation, on the tree leaf
+        level.
+
+    paths_ : Dict[int, Tuple[int]]
+        Describes how the cluster number corresponds to the path in the tree.
+        Element of the tuple indicates the sub-segment number on each tree
+        level.
+
+    reverse_paths_ : Dict[Tuple[int], int]
+        Describes how the path in the tree corresponds to the cluster number.
+        For more details see ``paths_``.
+
+    Examples
+    --------
+
+    >>> from divik import DiviK
+    >>> from sklearn.datasets import make_blobs
+    >>> X, _ = make_blobs(n_samples=200, n_features=100, centers=20,
+    ...                   random_state=42)
+    >>> divik = DiviK(distance='euclidean').fit(X)
+    >>> divik.labels_
+    array([1, 1, 1, 0, ..., 0, 0], dtype=int32)
+    >>> divik.predict([[0, ..., 0], [12, ..., 3]])
+    array([1, 0], dtype=int32)
+    >>> divik.cluster_centers_
+    array([[10., ...,  2.],
+           ...,
+           [ 1, ...,  2.]])
+
+    """
+
     def __init__(self,
                  gap_trials: int = 10,
                  distance_percentile: float = 99.,
@@ -48,6 +175,17 @@ class DiviK(BaseEstimator, ClusterMixin, TransformerMixin):
         self.verbose = verbose
 
     def fit(self, X, y=None):
+        """Compute DiviK clustering.
+
+        Parameters
+        ----------
+        X : array-like or sparse matrix, shape=(n_samples, n_features)
+            Training instances to cluster. It must be noted that the data
+            will be converted to C ordering, which will cause a memory
+            copy if the given data is not C-contiguous.
+        y : Ignored
+            not used, present here for API consistency by convention.
+        """
         n_cpu = os.cpu_count()
         n_jobs = 1 if self.n_jobs is None else self.n_jobs
         n_jobs = (n_jobs + n_cpu) % n_cpu or n_cpu
@@ -94,13 +232,70 @@ class DiviK(BaseEstimator, ClusterMixin, TransformerMixin):
         return self
 
     def fit_predict(self, X, y=None):
+        """Compute cluster centers and predict cluster index for each sample.
+
+        Convenience method; equivalent to calling fit(X) followed by
+        predict(X).
+
+        Parameters
+        ----------
+
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            New data to transform.
+
+        y : Ignored
+            not used, present here for API consistency by convention.
+
+        Returns
+        -------
+
+        labels : array, shape [n_samples,]
+            Index of the cluster each sample belongs to.
+        """
         return self.fit(X).labels_
 
     def fit_transform(self, X, y=None, **fit_params):
+        """Compute clustering and transform X to cluster-distance space.
+
+        Equivalent to fit(X).transform(X), but more efficiently implemented.
+
+        Parameters
+        ----------
+
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            New data to transform.
+
+        y : Ignored
+            not used, present here for API consistency by convention.
+
+        Returns
+        -------
+
+        X_new : array, shape [n_samples,]
+            X transformed in the new space.
+        """
         # TODO: optimize
         return self.fit(X).transform(X)
 
     def transform(self, X, with_path: bool = False):
+        """Transform X to a cluster-distance space.
+
+        In the new space, each dimension is the distance to the cluster
+        centers.  Note that even if X is sparse, the array returned by
+        `transform` will typically be dense.
+
+        Parameters
+        ----------
+
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            New data to transform.
+
+        Returns
+        -------
+
+        X_new : array, shape [n_samples,]
+            X transformed in the new space.
+        """
         if self.normalize_rows is None:
             if self.distance == dst.KnownMetric.correlation.value:
                 normalize_rows_ = True
@@ -139,5 +334,23 @@ class DiviK(BaseEstimator, ClusterMixin, TransformerMixin):
         return np.array(distances)
 
     def predict(self, X):
+        """Predict the closest cluster each sample in X belongs to.
+
+        In the vector quantization literature, `cluster_centers_` is called
+        the code book and each value returned by `predict` is the index of
+        the closest code in the code book.
+
+        Parameters
+        ----------
+
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            New data to predict.
+
+        Returns
+        -------
+
+        labels : array, shape [n_samples,]
+            Index of the cluster each sample belongs to.
+        """
         _, paths = self.transform(X, with_path=True)
         return np.array([self.reverse_paths_[path] for path in paths])
