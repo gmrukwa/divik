baee4f07680326c59a1143b6e2dd594980fcd4b9
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-10-26 14:36:24 +0200

    Fix readthedocs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -69,3 +69,6 @@ html_static_path = ['_static']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+# For ReadTheDocs according to https://stackoverflow.com/a/56448499
+master_doc = 'index'
