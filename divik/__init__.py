e62450d9d631dc5c07bcb559b134326e9e1c5819
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-10-26 13:05:58 +0200

    Include exported elements in top-level module __all__

--- divik/__init__.py
+++ divik/__init__.py
@@ -5,3 +5,11 @@ from ._sklearn import DiviK
 from ._kmeans import AutoKMeans, KMeans
 from ._feature_selection import GMMSelector, HighAbundanceAndVarianceSelector
 from ._summary import depth, plot, reject_split
+
+__all__ = [
+    "seeded",
+    "DiviK",
+    "AutoKMeans", "KMeans",
+    "GMMSelector", "HighAbundanceAndVarianceSelector",
+    "depth", "plot", "reject_split",
+]
