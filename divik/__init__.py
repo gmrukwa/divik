adc7d0aa5afdb44449499eae2546d8a45a2b8650
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-12-08 02:41:04 +0100

    Add feature selector based on Huberta outlier detection

--- divik/__init__.py
+++ divik/__init__.py
@@ -3,7 +3,12 @@ __version__ = '2.1.8'
 from ._seeding import seeded
 from ._sklearn import DiviK
 from ._kmeans import AutoKMeans, KMeans
-from ._feature_selection import GMMSelector, HighAbundanceAndVarianceSelector
+from ._feature_selection import (
+    GMMSelector,
+    HighAbundanceAndVarianceSelector,
+    huberta_outliers,
+    OutlierSelector
+)
 from ._summary import depth, plot, reject_split
 
 __all__ = [
@@ -12,5 +17,6 @@ __all__ = [
     "DiviK",
     "AutoKMeans", "KMeans",
     "GMMSelector", "HighAbundanceAndVarianceSelector",
+    'huberta_outliers', 'OutlierSelector',
     "depth", "plot", "reject_split",
 ]
