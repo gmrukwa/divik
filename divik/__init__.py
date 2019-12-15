4ce6a50528e776351d4505088add95156d74dd74
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-12-15 13:12:16 +0100

    Remove depth from public API

--- divik/__init__.py
+++ divik/__init__.py
@@ -3,12 +3,12 @@ __version__ = '2.3.5'
 from ._seeding import seeded
 from ._sklearn import DiviK
 from ._kmeans import AutoKMeans, KMeans
-from ._summary import depth, plot, reject_split
+from ._summary import plot, reject_split
 
 __all__ = [
     "__version__",
     "seeded",
     "DiviK",
     "AutoKMeans", "KMeans",
-    "depth", "plot", "reject_split",
+    "plot", "reject_split",
 ]
