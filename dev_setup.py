fdd42188ef25f07eb7d2f1bdaca351e0fcdee36b
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-12-26 22:48:45 +0100

    Install gamred_native to the dev image

---
+++ dev_setup.py
@@ -0,0 +1,19 @@
+"""This just builds the native extension for testing purposes"""
+
+from glob import glob
+from setuptools import setup, Extension
+import numpy
+
+setup(
+    packages=[],
+    # @gmrukwa: https://packaging.python.org/discussions/install-requires-vs-requirements/
+    install_requires=[
+        'numpy>=0.12.1',
+    ],
+    python_requires='>=3.5,<=3.7',
+    ext_modules=[
+        Extension('gamred_native',
+                  sources=glob('gamred_native/*.c'),
+                  include_dirs=['gamred_native', numpy.get_include()])
+    ],
+)
--- docker/development.dockerfile
+++ docker/development.dockerfile
@@ -13,3 +13,13 @@ RUN pip install --no-cache-dir -r /requirements.txt &&\
 
 RUN mkdir -p /root/.config/matplotlib &&\
   echo "backend : Agg" > /root/.config/matplotlib/matplotlibrc
+
+WORKDIR /app
+
+COPY gamred_native /app/gamred_native
+
+COPY dev_setup.py /app/dev_setup.py
+
+RUN python dev_setup.py install &&\
+    rm -rf /app/gamred_native &&\
+    rm /app/dev_setup.py
