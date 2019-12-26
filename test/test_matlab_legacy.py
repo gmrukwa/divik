350f3280d73c2cd42ce73302120e8507868d9cf0
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-12-26 22:58:16 +0100

    Test native thresholds finding

--- test/test_matlab_legacy.py
+++ test/test_matlab_legacy.py
@@ -29,3 +29,29 @@ class TestFindThresholds(unittest.TestCase):
         self.assertGreater(thresholds[0], -100)
         self.assertLess(thresholds[1], 100)
         self.assertGreater(thresholds[1], 0)
+
+
+class TestFindThresholdsNative(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(0)
+
+    def test_separates_two_components(self):
+        first = np.random.randn(100)
+        second = 100 + 2 * np.random.randn(100)
+        values = np.hstack((first, second))
+        thresholds = ml.find_thresholds_native(values, max_components=2)
+        self.assertEqual(len(thresholds), 1)
+        self.assertLess(thresholds[0], 100)
+        self.assertGreater(thresholds[0], 0)
+
+    def test_separates_multiple_components(self):
+        first = np.random.randn(1000)
+        second = 100 + 2 * np.random.randn(1000)
+        third = -second
+        values = np.hstack((first, -first, second, third))
+        thresholds = ml.find_thresholds_native(values, max_components=3)
+        self.assertEqual(len(thresholds), 2)
+        self.assertLess(thresholds[0], 0)
+        self.assertGreater(thresholds[0], -100)
+        self.assertLess(thresholds[1], 100)
+        self.assertGreater(thresholds[1], 0)
