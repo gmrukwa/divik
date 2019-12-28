de6a63459eae418b7ced3951adf6f0816149120e
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-12-29 00:21:26 +0100

    Switch Linux build to manylinux config

--- .github/actions/build-python-dist/entrypoint.sh
+++ .github/actions/build-python-dist/entrypoint.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-pip install --no-cache-dir -r requirements.txt
+/opt/python/cp35-cp35m/bin/pip install -r requirements-base.txt
+/opt/python/cp35-cp35m/bin/python setup.py sdist
+/opt/python/cp35-cp35m/bin/pip wheel . -w dist/tmp
+auditwheel repair dist/tmp/divik*whl -w dist
+rm -rf dist/tmp
 
-python setup.py sdist
+/opt/python/cp36-cp36m/bin/pip install -r requirements-base.txt
+/opt/python/cp36-cp36m/bin/pip wheel . -w dist/tmp
+auditwheel repair dist/tmp/divik*whl -w dist
+rm -rf dist/tmp
+
+/opt/python/cp37-cp37m/bin/pip install -r requirements-base.txt
+/opt/python/cp37-cp37m/bin/pip wheel . -w dist/tmp
+auditwheel repair dist/tmp/divik*whl -w dist
+rm -rf dist/tmp
+
+/opt/python/cp38-cp38/bin/pip install -r requirements-base.txt
+/opt/python/cp38-cp38/bin/pip wheel . -w dist/tmp
+auditwheel repair dist/tmp/divik*whl -w dist
+rm -rf dist/tmp
 
 ls dist
