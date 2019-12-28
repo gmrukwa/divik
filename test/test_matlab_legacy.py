270c426fb98e2b426622eb3a9804f34b42d06a4c
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-12-28 18:33:58 +0100

    Add tests for consistency between MCR and native versions

--- test/test_matlab_legacy.py
+++ test/test_matlab_legacy.py
@@ -1,8 +1,37 @@
 import unittest
+from parameterized import parameterized
 
 import numpy as np
+import numpy.testing as npt
+from sklearn.datasets import make_blobs
 
 import divik._matlab_legacy as ml
+
+
+def no_threshold():
+    return np.random.randn(500)
+
+
+def single_th_between_0_and_100():
+    first = np.random.randn(100)
+    second = 100 + 2 * np.random.randn(100)
+    values = np.hstack((first, second))
+    return values
+
+
+def two_ths_between_minus_100_0_and_100():
+    first = np.random.randn(1000)
+    second = 100 + 2 * np.random.randn(1000)
+    third = -second
+    values = np.hstack((first, -first, second, third))
+    return values
+
+
+def blobs(n, seed=0):
+    def some_blobs():
+        return make_blobs(n_samples=500, centers=n, n_features=1,
+                          random_state=seed)[0].ravel()
+    return some_blobs
 
 
 class TestFindThresholdsMcr(unittest.TestCase):
@@ -10,19 +39,14 @@ class TestFindThresholdsMcr(unittest.TestCase):
         np.random.seed(0)
 
     def test_separates_two_components(self):
-        first = np.random.randn(100)
-        second = 100 + 2 * np.random.randn(100)
-        values = np.hstack((first, second))
+        values = single_th_between_0_and_100()
         thresholds = ml.find_thresholds_mcr(values, max_components=2)
         self.assertEqual(len(thresholds), 1)
         self.assertLess(thresholds[0], 100)
         self.assertGreater(thresholds[0], 0)
 
     def test_separates_multiple_components(self):
-        first = np.random.randn(1000)
-        second = 100 + 2 * np.random.randn(1000)
-        third = -second
-        values = np.hstack((first, -first, second, third))
+        values = two_ths_between_minus_100_0_and_100()
         thresholds = ml.find_thresholds_mcr(values, max_components=3)
         self.assertEqual(len(thresholds), 2)
         self.assertLess(thresholds[0], 0)
@@ -31,27 +55,49 @@ class TestFindThresholdsMcr(unittest.TestCase):
         self.assertGreater(thresholds[1], 0)
 
 
-class TestFindThresholds(unittest.TestCase):
+class TestFindThresholdsNative(unittest.TestCase):
     def setUp(self):
         np.random.seed(0)
 
     def test_separates_two_components(self):
-        first = np.random.randn(100)
-        second = 100 + 2 * np.random.randn(100)
-        values = np.hstack((first, second))
+        values = single_th_between_0_and_100()
         thresholds = ml.find_thresholds_native(values, max_components=2)
         self.assertEqual(len(thresholds), 1)
         self.assertLess(thresholds[0], 100)
         self.assertGreater(thresholds[0], 0)
 
     def test_separates_multiple_components(self):
-        first = np.random.randn(1000)
-        second = 100 + 2 * np.random.randn(1000)
-        third = -second
-        values = np.hstack((first, -first, second, third))
+        values = two_ths_between_minus_100_0_and_100()
         thresholds = ml.find_thresholds_native(values, max_components=3)
         self.assertEqual(len(thresholds), 2)
         self.assertLess(thresholds[0], 0)
         self.assertGreater(thresholds[0], -100)
         self.assertLess(thresholds[1], 100)
         self.assertGreater(thresholds[1], 0)
+
+
+class TestFindThresholdsConsistency(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(0)
+
+    @parameterized.expand([(f.__name__, f()) for f in [
+        no_threshold,
+        single_th_between_0_and_100,
+        two_ths_between_minus_100_0_and_100,
+        blobs(5),
+        blobs(7),
+        blobs(10),
+        blobs(13),
+        blobs(17),
+        blobs(20),
+        blobs(5, seed=123),
+        blobs(7, seed=123),
+        blobs(10, seed=123),
+        blobs(13, seed=123),
+        blobs(17, seed=123),
+        blobs(20, seed=123),
+    ]])
+    def test_consistency(self, _, values):
+        native = ml.find_thresholds_native(values)
+        mcr = ml.find_thresholds_mcr(values)
+        npt.assert_almost_equal(native, mcr, decimal=3)
