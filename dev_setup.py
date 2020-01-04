20c570b49de585890abdb9f2c0a97147a2ca863b
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2020-01-04 15:23:43 +0100

    Add name and version to dev_setup

--- dev_setup.py
+++ dev_setup.py
@@ -3,8 +3,11 @@
 from glob import glob
 from setuptools import setup, Extension
 import numpy
+from divik import __version__
 
 setup(
+    name='gamred_native',
+    version=__version__,
     packages=[],
     # @gmrukwa: https://packaging.python.org/discussions/install-requires-vs-requirements/
     install_requires=[
