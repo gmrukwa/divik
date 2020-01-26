adb5aa87835b2efa28b65dda8961adbce499330a
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2020-01-26 22:32:18 +0100

    Stop importing modules to top level

--- divik/__init__.py
+++ divik/__init__.py
@@ -1,19 +1,9 @@
 __version__ = '2.4.1'
 
-from divik import core
-from divik import feature_selection
-from divik import feature_extraction
-from divik import cluster
-from divik import sampler
 from ._summary import plot, reject_split
 
 
 __all__ = [
     "__version__",
-    "core",
-    "cluster",
-    "feature_selection",
-    "feature_extraction",
-    "sampler",
     "plot", "reject_split",
 ]
