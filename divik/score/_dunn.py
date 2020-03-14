e592015457c342ce7e496ff814e554fab3ee685a
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2020-03-14 20:10:33 +0100

    Introduce new Dunn variants

--- divik/score/_dunn.py
+++ divik/score/_dunn.py
@@ -2,13 +2,55 @@ import numpy as np
 import pandas as pd
 from scipy.spatial import distance as dist
 
-from divik.core import Data
+from divik.core import configurable, Data
 
 
-KMeans = 'divik.KMeans'
+KMeans = 'divik.cluster.KMeans'
 
 
-def dunn(kmeans: KMeans, data: Data) -> float:
+def _inter_centroid(kmeans: KMeans, data: Data):
+    d = dist.pdist(kmeans.cluster_centers_, kmeans.distance)
+    return np.min(d[d != 0])
+
+
+def _inter_closest(kmeans: KMeans, data: Data):
+    d = np.inf
+    for label in np.arange(kmeans.n_clusters - 1):
+        grp = label == kmeans.labels_
+        non_grp = label < kmeans.labels_
+        dst = dist.cdist(data[grp], data[non_grp], metric=kmeans.distance)
+        d = np.minimum(d, dst.min())
+    return d
+
+
+def _intra_avg(kmeans: KMeans, data: Data):
+    clusters = pd.DataFrame(data).groupby(kmeans.labels_).apply(np.asarray)
+    return np.max([
+        np.mean(dist.cdist(cluster, centroid.reshape(1, -1), kmeans.distance))
+        for cluster, centroid in zip(clusters, kmeans.cluster_centers_)
+    ])
+
+
+def _intra_furthest(kmeans: KMeans, data: Data):
+    def max_distance(group):
+        group = np.asarray(group)
+        d = dist.pdist(group, metric=kmeans.distance)
+        return np.max(d)
+    return pd.DataFrame(data).groupby(kmeans.labels_).apply(max_distance).max()
+
+
+_INTER = {
+    'centroid': _inter_centroid,
+    'closest': _inter_closest,
+}
+_INTRA = {
+    'avg': _intra_avg,
+    'furthest': _intra_furthest,
+}
+
+
+@configurable
+def dunn(kmeans: KMeans, data: Data, inter='centroid', intra='avg') -> float:
     """Compute Dunn's index for the clustering
 
     Parameters
@@ -19,6 +61,16 @@ def dunn(kmeans: KMeans, data: Data) -> float:
     data : array, shape (n_samples, n_features)
         Clustered data
 
+    inter : {'centroid', 'closest'}
+        Method of computing intercluster distance
+        - centroid - uses distances between centroids
+        - closest - uses distance between closest members of separate clusters
+
+    intra : {'avg', 'furthest}
+        Method of computing intracluster distance
+        - avg - uses average distance to the centroid
+        - furthest - uses distance between the furthest cluster members
+
     Returns
     -------
     dunn_index : float
@@ -26,12 +78,13 @@ def dunn(kmeans: KMeans, data: Data) -> float:
     """
     if kmeans.cluster_centers_.shape[0] == 1:
         return -np.inf
-    clusters = pd.DataFrame(data).groupby(kmeans.labels_).apply(np.asarray)
-    intercluster = dist.pdist(kmeans.cluster_centers_, kmeans.distance)
-    intercluster = np.min(intercluster[intercluster != 0])
-    intracluster = np.max([
-        np.mean(dist.cdist(cluster, centroid.reshape(1, -1), kmeans.distance))
-        for cluster, centroid in zip(clusters, kmeans.cluster_centers_)
-    ])
+    if inter not in _INTER:
+        raise ValueError(f'Unsupported intercluster distance {inter}. '
+                         f'Supported: {list(_INTER.keys())}')
+    if intra not in _INTRA:
+        raise ValueError(f'Unsupported intracluster distance {intra}. '
+                         f'Supported: {list(_INTRA.keys())}')
+    intercluster = _INTER[inter](kmeans, data)
+    intracluster = _INTRA[intra](kmeans, data)
     score = intercluster / intracluster
     return score
--- test/score/test_dunn.py
+++ test/score/test_dunn.py
@@ -2,13 +2,42 @@ import unittest
 
 import numpy as np
 
-from divik.score._dunn import dunn
+from divik.score._dunn import (
+    _inter_centroid,
+    _inter_closest,
+    _intra_avg,
+    _intra_furthest,
+    dunn,
+)
 
 
 class DummyKMeans:
+    n_clusters = 2
     cluster_centers_ = np.array([[2], [5]])
-    labels_ = np.array([1, 1, 2, 2], dtype=int)
+    labels_ = np.array([0, 0, 1, 1], dtype=int)
     distance = 'euclidean'
+
+
+class TestDunnDistances(unittest.TestCase):
+    def test_inter_centroid(self):
+        data = np.array([[1], [3], [4], [6]])
+        dst = _inter_centroid(DummyKMeans(), data)
+        self.assertAlmostEqual(dst, 3.)
+
+    def test_inter_closest(self):
+        data = np.array([[1], [3], [4], [6]])
+        dst = _inter_closest(DummyKMeans(), data)
+        self.assertAlmostEqual(dst, 1.)
+
+    def test_intra_avg(self):
+        data = np.array([[1], [3], [4], [6]])
+        dst = _intra_avg(DummyKMeans(), data)
+        self.assertAlmostEqual(dst, 1.)
+
+    def test_intra_furthest(self):
+        data = np.array([[1], [3], [4], [6]])
+        dst = _intra_furthest(DummyKMeans(), data)
+        self.assertAlmostEqual(dst, 2.)
 
 
 class TestDunn(unittest.TestCase):
@@ -16,3 +45,8 @@ class TestDunn(unittest.TestCase):
         data = np.array([[1], [3], [4], [6]])
         dunn_ = dunn(DummyKMeans(), data)
         self.assertAlmostEqual(dunn_, 3.)
+
+    def test_works_with_other_distances(self):
+        data = np.array([[1], [3], [4], [6]])
+        dunn_ = dunn(DummyKMeans(), data, inter='closest', intra='furthest')
+        self.assertAlmostEqual(dunn_, .5)
